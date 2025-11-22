9db543fabf93d767d33711189a719a38e8fb5860
Karthicksk0 <132269889+Karthicksk0@users.noreply.github.com>
2025-11-22 23:23:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require("http");
 const PORT = 3000;
 
 http.createServer((req, res) => {
-  res.end("Hello from Jenkins CI/CD Pipeline! - Antony george ");
+  res.end("Hello from Jenkins CI/CD Pipeline! - Karthick ");
 }).listen(PORT, () => {
   console.log("Server running on port", PORT);
 });
